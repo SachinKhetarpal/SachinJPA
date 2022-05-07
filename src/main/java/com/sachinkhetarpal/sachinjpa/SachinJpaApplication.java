c4a2998ab2b71edb790fcce882b52809ab650e3b
SachinKhetarpal <sachin0993@gmail.com>
2022-05-07 14:18:51 +0530

    initial code

---
+++ src/main/java/com/sachinkhetarpal/sachinjpa/SachinJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sachinkhetarpal.sachinjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SachinJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SachinJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sachinkhetarpal/sachinjpa/SachinJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinkhetarpal.sachinjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SachinJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
